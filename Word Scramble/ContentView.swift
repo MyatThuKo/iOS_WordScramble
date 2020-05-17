879597aa5f1a2da2de3ac6f1b02f59ca611b77bc
Myat Thu Ko <nymyatthuko@gmail.com>
2020-05-17 17:31:59 -0400

    Initial Commit

---
+++ Word Scramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by Myat Thu Ko on 5/17/20.
+//  Copyright © 2020 Myat Thu Ko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
